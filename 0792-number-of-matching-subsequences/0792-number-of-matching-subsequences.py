b462827929dcb0bb113bd6ff7b02c6b6fc862691
Sudesh Patil <SUDESHRPATIL201@GMAIL.COM>
2025-12-06 13:58:17 +0530

    Time: 1626 ms (5.03%), Space: 19.3 MB (94.69%) - LeetHub

---
+++ 0792-number-of-matching-subsequences/0792-number-of-matching-subsequences.py
@@ -0,0 +1,45 @@
+class Solution:
+    def numMatchingSubseq(self, s: str, words: List[str]) -> int:
+        cache = {}     # memoization cache
+        count = 0
+
+        def is_subseq(w):
+            i = j = 0
+            while i < len(s) and j < len(w):
+                if s[i] == w[j]:
+                    j += 1
+                i += 1
+            return j == len(w)
+
+        for w in words:
+            if w in cache:
+                count += cache[w]
+            else:
+                res = 1 if is_subseq(w) else 0
+                cache[w] = res
+                count += res
+
+        return count
+
+
+
+
+
+        # if len(words) < 1:
+        #     return 0
+        # char = words[0]
+        # i = 0
+        # j = 0
+        # while j < len(char) and i < len(s):
+        #     if s[i] == char[j]:
+        #         i += 1
+        #         j += 1
+        #     else:
+        #         i += 1
+
+        # if  j == len(char):
+        #     count = 1
+        # else:
+        #     count = 0
+        # return count + self.numMatchingSubseq(s, words[1:])
+        
