a70362ff33efd34b67ddb2d8b16ebf2870b3fccb
Sudesh Patil <SUDESHRPATIL201@GMAIL.COM>
2025-12-15 18:16:35 +0530

    Time: 0 ms (100%), Space: 17.8 MB (36.33%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.py
@@ -0,0 +1,20 @@
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        words=s.split()
+
+        if len(pattern) != len(words):
+            return False
+        
+        dic={}
+        used_words=set()
+
+        for i in range(len(pattern)):
+            if pattern[i] in dic:
+                if dic[pattern[i]] != words[i]:
+                    return False
+            else:
+                if words[i] in used_words:
+                    return False
+                dic[pattern[i]] = words[i]
+                used_words.add(words[i])
+        return True
