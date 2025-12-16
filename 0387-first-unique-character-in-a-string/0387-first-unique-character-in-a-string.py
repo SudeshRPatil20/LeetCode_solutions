df5bf1e0790f4115a06147723bd0fc20b78bcdb5
Sudesh Patil <SUDESHRPATIL201@GMAIL.COM>
2025-12-16 16:31:25 +0530

    Time: 54 ms (68.54%), Space: 17.9 MB (96.8%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.py
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.py
@@ -1,20 +1,45 @@
 class Solution:
     def firstUniqChar(self, s: str) -> int:
-        i = 0
-        
-        while i < len(s):
-            j = 0
-            is_unique=True
+        # this is by hashmap
+        dic={}
 
-            while j < len(s):
-                if i != j and s[i] == s[j]:
-                    is_unique = False
-                    break
-                j +=1
-
-            if is_unique:
-                return i
+        for char in s:
+            if char in dic:
+                dic[char] += 1
+            else:
+                dic[char] = 1
             
-            i += 1
+        for char in dic:
+            if dic[char] == 1:
+                return s.index(char)
+        return -1
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # this is 2 pointer
+        # i = 0
+        
+        # while i < len(s):
+        #     j = 0
+        #     is_unique=True
+
+        #     while j < len(s):
+        #         if i != j and s[i] == s[j]:
+        #             is_unique = False
+        #             break
+        #         j +=1
+
+        #     if is_unique:
+        #         return i
+            
+        #     i += 1
             
         return -1
