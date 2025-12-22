321f99f39e79b18468aa776e683655191afa1b76
Sudesh Patil <SUDESHRPATIL201@GMAIL.COM>
2025-12-22 16:11:48 +0530

    Time: 3 ms (83.37%), Space: 19.3 MB (85.76%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,9 +1,23 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        a={}
-        for num in nums:
-            if num in a:
-                a[num] +=1
+        # a={}
+        # for num in nums:
+        #     if num in a:
+        #         a[num] +=1
+        #     else:
+        #         a[num] = 1
+        # return max(a, key=a.get)
+
+        # without hashmap
+        vote_count=0
+        candidate= 0
+
+        for i in range(len(nums)):
+            current_num=nums[i]
+            if vote_count == 0:
+                candidate= current_num
+            if current_num == candidate:
+                vote_count += 1
             else:
-                a[num] = 1
-        return max(a, key=a.get)
+                vote_count -= 1
+        return candidate
