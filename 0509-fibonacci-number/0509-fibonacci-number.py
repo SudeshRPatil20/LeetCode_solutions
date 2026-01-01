d27bcfe9fe3e19b96585838dd1e5b5e80260ae1e
Sudesh Patil <SUDESHRPATIL201@GMAIL.COM>
2026-01-01 20:54:20 +0530

    Time: 36 ms (74.63%), Space: 17.4 MB (82.88%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -0,0 +1,26 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        diry=[-1] * (n+1)
+        # if n <= 1:
+        #     return n
+        # return self.fib(n-1) + self.fib(n-2)
+    # this is not optimize because it gives complexity of 2^n
+    # therefore the best approach is as follows (note do this infront of interviewer
+        def fib_helper(n):
+
+            if (n <= 1):
+                return n
+            
+            if diry[n] != -1:
+                return diry[n]
+            
+            fib1=fib_helper(n-1)
+            fib2 = fib_helper(n-2)
+
+            diry[n-1] = fib1
+            diry[n-2] = fib2
+
+            ans = fib1 + fib2
+            diry[n] = ans
+            return diry[n]
+        return fib_helper(n)
